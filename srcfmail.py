76392ba6277f6e0f699604d5ed20ff2f23bfa190
Ben Millwood <bm380@srcf.net>
2012-03-28 17:10:49 +0000

    Document some stuff about mailtosysadmins

--- srcfmail.py
+++ srcfmail.py
@@ -26,6 +26,19 @@ def pretty_sysadmin_name(name):
     return name.split(',')[0].split('(')[0].rstrip()
 
 def mailtosysadmins(subject, body):
+    """Send a mail to SYSADMINEMAIL with the given subject and body,
+    which should both be strings.
+
+    From: if bm380-adm does 'sudo srcf-mailtosysadmins subject' then the
+    from will be "Ben Millwood <bm380@srcf.net>" – i.e. take SUDO_USER (or
+    getuid), strip -adm if present, add @srcf.net, lookup name and apply
+    pretty_sysadmin_name.
+
+    Note that this differs from e.g. the srcf-mailtouser script, which puts
+    soc-srcf-admin in the From field (even though it *does* work out who you
+    are, so it can sign emails as you).
+    
+    Content-type: text/plain; format=flowed; charset=UTF-8"""
     sender = whoami()
     fromaddr = sender.pw_name.replace('-adm','') + '@srcf.net'
     myname = pretty_sysadmin_name(sender.pw_gecos)
