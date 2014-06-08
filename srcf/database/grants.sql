acfc6c0b52157cff657cdf52999fb2e036a0e3d6
Daniel Richman <main@danielrichman.co.uk>
2014-06-08 15:10:51 +0000

    Allow sysadmins to dump the database (sbin/member_backup)

--- srcf/database/grants.sql
+++ srcf/database/grants.sql
@@ -4,3 +4,11 @@ GRANT USAGE ON SCHEMA PUBLIC TO nobody;
 GRANT SELECT (crsid, surname, preferred_name, email, joined, modified, member, "user") ON members TO nobody;
 GRANT SELECT (society, description, joined, modified) ON societies TO nobody;
 GRANT SELECT ON society_admins TO nobody;
+
+-- sysadmins uses pg_dump to back up the database
+GRANT USAGE ON SCHEMA PUBLIC TO sysadmins;
+GRANT SELECT ON members TO sysadmins;
+GRANT SELECT ON societies TO sysadmins;
+GRANT SELECT ON society_admins TO sysadmins;
+GRANT SELECT ON log TO sysadmins;
+GRANT SELECT ON log_record_id_seq TO sysadmins;
