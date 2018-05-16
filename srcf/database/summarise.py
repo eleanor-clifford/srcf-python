3cf7a403884e57ef7c4488fd02139767496f2459
Richard Allitt <rsa33@srcf.net>
2018-05-16 18:20:52 +0000

    srcf-who: Fix queries and summary display

--- srcf/database/summarise.py
+++ srcf/database/summarise.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 import textwrap
 
-from . import Member, Society
+from . import Member, Society, RESTRICTED
 
 def summarise_member(member):
     """
@@ -19,16 +19,19 @@ def summarise_member(member):
     else:
         socs = ["No society memberships."]
 
-    joined = member.joined.strftime("%Y/%m")
+    private = []
+    if not RESTRICTED:
+        private = [member.email,
+                   'Member: %s' % member.member,
+                   'User: %s' % member.user,
+                   'Joined: %s' % member.joined.strftime("%Y/%m"),
+                   'Danger: %s' % member.danger] \
+                + _format_notes(member.notes)
 
     lines = \
-        ['%s (%s)' % (member.name, member.crsid),
-         member.email,
-         'Member: %s' % member.member,
-         'User: %s' % member.user,
-         'Joined: %s' % joined] \
-      + socs \
-      + _maybe_sysadmin_info(member)
+        ['%s (%s)' % (member.name, member.crsid)] \
+      + private \
+      + socs
 
     return "\n".join(lines)
 
@@ -41,12 +44,15 @@ def summarise_society(society):
     else:
         admins = ['Orphaned (no admins).']
 
-    joined = society.joined.strftime("%Y/%m")
+    private = []
+    if not RESTRICTED:
+        private = ['Joined: %s' % society.joined.strftime("%Y/%m"),
+                   'Danger: %s' % society.danger] \
+                + _format_notes(society.notes)
 
-    lines = ['%s: %s' % (society.society, society.description),
-             'Joined: %s' % joined] \
-            + admins \
-            + _maybe_sysadmin_info(society)
+    lines = ['%s: %s' % (society.society, society.description)] \
+            + private \
+            + admins
 
     return "\n".join(lines)
 
@@ -92,13 +98,10 @@ def _pretty_name_list(names):
 	return ['  %-*s  (%s)' % (maxlen, col1, col2)
 		    for (col1, col2) in nameList]
 
-def _maybe_sysadmin_info(thing):
+def _format_notes(notes):
     def indent(n): return "\n".join("  " + l for l in n.splitlines())
-
     s = []
-    if hasattr(thing, "danger"):
-        s.append("Danger: %s" % thing.danger)
-    if hasattr(thing, "notes") and thing.notes:
+    if notes:
         tw = textwrap.TextWrapper(width=70, initial_indent='  ', subsequent_indent='  ')
         s.append("Notes:")
         for line in thing.notes.splitlines():
