2ded02acc2173a443c1ae998a2fe04e28410b52f
Malcolm Scott <github@malc.org.uk>
2019-06-09 18:20:43 +0000

    If we let srcf.database.queries auto-create a session, use autocommit and explicit transactions so that we don't leave transactions dangling.

--- srcf/database/queries.py
+++ srcf/database/queries.py
@@ -1,4 +1,5 @@
 import warnings
+from contextlib import contextmanager
 
 from . import Member, Society, Session
 
@@ -9,15 +10,29 @@ _auto_create_global_session = True
 # We can't create temporary session objects since this breaks lazy loading.
 # When the session goes out of scope, the Member/Society objects seem to lose
 # it (? weakrefs) so can't lazy load (.admins, .societies)
+#
+# We only do read-only actions here, and don't want to leave a transaction
+# open indefinitely, so we use autocommit.  Note documented caveats at
+# https://docs.sqlalchemy.org/en/13/orm/session_transaction.html#autocommit-mode
+# -- mas90 asserts that our use case here falls under "framework integrations
+# that wish to control specifically when the 'begin' state occurs".
+#
+# Because we use autocommit, we REALLY OUGHT TO explicitly start transactions
+# for EVERY query (and yes, it's OK to do so even if we're already in a
+# transaction in an external session -- SQLAlchemy supports nested transactions).
+@contextmanager
 def _sess(session=None):
     global _global_session, _auto_create_global_session
     if session:
-        return session
+        with session.begin():
+            yield session
     elif _global_session:
-        return _global_session
+        with _global_session.begin():
+            yield _global_session
     elif _auto_create_global_session:
-        _global_session = Session()
-        return _global_session
+        _global_session = Session(autocommit=True)
+        with _global_session.begin():
+            yield _global_session
     else:
         raise RuntimeError("Auto global session creation is disabled")
 
@@ -32,40 +47,49 @@ def disable_automatic_session(and_use_this_one_instead=None):
 
 
 def list_members(session=None):
-    return _sess(session).query(Member)
+    with _sess(session) as sess:
+        return sess.query(Member)
 
 def get_member(crsid, session=None):
-    m = _sess(session).query(Member).get(crsid)
-    if m:
-        return m
-    else:
-        raise KeyError(crsid)
+    with _sess(session) as sess:
+        m = sess.query(Member).get(crsid)
+        if m:
+            return m
+        else:
+            raise KeyError(crsid)
 
 def list_users(session=None):
-    return _sess(session).query(Member).filter(Member.user==True)
+    with _sess(session) as sess:
+        return sess.query(Member).filter(Member.user==True)
 
 def get_user(crsid, session=None):
-    m = get_member(crsid, session)
-    if not m.user:
-        raise KeyError(crsid)
-    else:
-        return m
+    with _sess(session) as sess:
+        m = get_member(crsid, session)
+        if not m.user:
+            raise KeyError(crsid)
+        else:
+            return m
 
 def list_societies(session=None):
-    return _sess(session).query(Society)
+    with _sess(session) as sess:
+        return sess.query(Society)
 
 def get_society(name, session=None):
-    s = _sess(session).query(Society).get(name)
-    if s:
-        return s
-    else:
-        raise KeyError(name)
+    with _sess(session) as sess:
+        s = sess.query(Society).get(name)
+        if s:
+            return s
+        else:
+            raise KeyError(name)
 
 def dict_users(session = None):
-    return {m.crsid: m for m in _sess(session).query(Member).filter(Member.user == True)}
+    with _sess(session) as sess:
+        return {m.crsid: m for m in sess.query(Member).filter(Member.user == True)}
 
 def dict_members(session=None):
-    return {m.crsid: m for m in _sess(session).query(Member)}
+    with _sess(session) as sess:
+        return {m.crsid: m for m in sess.query(Member)}
 
 def dict_societies(session=None):
-    return {m.society: m for m in _sess(session).query(Society)}
+    with _sess(session) as sess:
+        return {m.society: m for m in sess.query(Society)}
