aeaa5bdd76322c4e4330cb32a9c72b5495f1578c
Richard Allitt <rsa33@srcf.net>
2019-11-03 18:31:00 +0000

    Add stub setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(name="srcflib",
+      description="Tools and helpers for the Student-Run Computing Facility.",
+      author="SRCF Sysadmins",
+      author_email="sysadmins@srcf.net",
+      url="https://www.srcf.net",
+      packages=["srcflib", "srcflib.plumbing", "srcflib.tasks"])
