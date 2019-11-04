e0aef54ec44164a1599c17f79144864c3b63f695
Richard Allitt <rsa33@srcf.net>
2019-11-04 21:58:45 +0000

    domains: Check IPv4/IPv6 separately using http.client

--- srcf/domains.py
+++ srcf/domains.py
@@ -2,44 +2,63 @@
 srcf.domains: Utilities for verifying domain ownership.
 """
 
-import binascii
+from binascii import hexlify
+from collections import defaultdict
+from contextlib import contextmanager
+from http.client import HTTPConnection
 import os
-import tempfile
+import socket
 from tempfile import NamedTemporaryFile
 
-import requests
 
-from srcf.database import Domain, Session
-
+HOST_DOMAIN = "srcf.societies.cam.ac.uk"
 
 WELL_KNOWN = "/public/societies/srcf-admin/srcf-well-known"
 
 
-def verify(domain):
-    """
-    Check if the given domain is correctly configured to serve an SRCF site.
-
-    A one-off challenge will be created and retrieved via /.well-known/srcf.
-
-    Returns `True` if valid, `False` if not, `None` if we couldn't connect.
-    """
+@contextmanager
+def make_challenge(domain):
     # Generate a random challenge value to ensure we're talking to ourselves.
-    challenge = binascii.hexlify(os.urandom(32))
-    proof = tempfile.NamedTemporaryFile(prefix="domain-{}-".format(domain),
-                                        dir=WELL_KNOWN)
+    challenge = hexlify(os.urandom(32))
+    proof = NamedTemporaryFile(prefix="domain-{}-".format(domain), dir=WELL_KNOWN)
     try:
         # Temp files are 0600 by default.  As it'll be owned by srcf-admin or
         # root, Apache won't have permission to read it as the site user.
         os.chmod(proof.name, 0o644)
         proof.write(challenge)
         proof.flush()
-        # Read the temp file back via the given domain.
-        url = ("http://{}/.well-known/srcf/{}"
-               .format(domain, os.path.basename(proof.name)))
-        try:
-            response = requests.get(url, timeout=5)
-            return response.content == challenge
-        except requests.ConnectionError:
-            return None
+        yield challenge, os.path.basename(proof.name)
     finally:
         proof.close()
+
+
+def get_server_ips():
+    server = "{}.{}".format(socket.gethostname(), HOST_DOMAIN)
+    raw = socket.getaddrinfo(server, "http", proto=socket.IPPROTO_TCP)
+    for family, *_, (ip, *_) in raw:
+        yield family, ip
+
+
+def verify(domain):
+    """
+    Check if the given domain is correctly configured to serve an SRCF website.
+    A one-off challenge will be created and retrieved via /.well-known/srcf.
+
+    Returns a tuple of values, `(IPv4, IPv6)`, where a given value can be
+    `True` if valid, `False` if not, or `None` if we couldn't connect.
+    """
+    with make_challenge(domain) as (challenge, filename):
+        # Read the temp file back via the given domain.
+        path = "/.well-known/srcf/{}".format(filename)
+        results = {}
+        for family, source in get_server_ips():
+            if family in results:
+                continue
+            try:
+                conn = HTTPConnection(domain, timeout=5, source_address=(source, 0))
+                conn.request("GET", path)
+                resp = conn.getresponse().read()
+                results[family] = resp == challenge
+            except socket.error:
+                results[family] = None
+        return results.get(socket.AF_INET), results.get(socket.AF_INET6)
