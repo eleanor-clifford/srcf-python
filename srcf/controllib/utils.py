74b781092b1a09ffa96f2973358a9d6697877386
Edwin Balani <git@balani.xyz>
2021-02-25 12:44:58 +0000

    Revert removal of ldapsearch() utility function
    The control panel (control.webapp) actually depends on this; we had
    missed this.
    
    Revert these commits:
    
      * adf8cc5a8edead562dd1c4f400bb3bfdb4decbdd
        Drop python-ldap3 from requirements
    
      * 7bf19a3b36b656a1839db9acc49248b8ccada3fc.
        Drop unused LDAP helper

--- srcf/controllib/utils.py
+++ srcf/controllib/utils.py
@@ -1,16 +1,30 @@
 import os
 import re
+from ldap3 import Server, Connection, ALL, ALL_ATTRIBUTES
 import stat
 import shutil
 import configparser
 import pymysql
 
 
-__all__ = ["email_re", "is_admin", "mysql_conn"]
+__all__ = ["email_re", "ldapsearch", "is_admin", "mysql_conn"]
 
 
 # yeah whatever.
 email_re = re.compile(r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z][A-Za-z]+$")
+
+
+# LDAP helper
+def ldapsearch(crsid):
+    server = Server('ldap.lookup.cam.ac.uk', get_info=ALL)
+    conn = Connection(server, auto_bind=True)
+    conn.search('ou=people, o=University of Cambridge,dc=cam,dc=ac,dc=uk',
+                '(uid={0})'.format(crsid), attributes=ALL_ATTRIBUTES)
+    r = conn.entries
+
+    if len(r) != 1:
+        raise KeyError(crsid)
+    return r[0]
 
 
 def is_admin(member):
