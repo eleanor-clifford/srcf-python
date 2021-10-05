c567ed59f50249661e2d1e946ea589ab118b895d
Richard Allitt <rsa33@srcf.net>
2021-10-05 22:29:55 +0100

    Add script to cancel user accounts

--- srcflib/scripts/member.py
+++ srcflib/scripts/member.py
@@ -2,6 +2,8 @@
 Scripts to manage users.
 """
 
+from sqlalchemy.orm import Session
+
 from srcf.database.schema import Member
 
 from .utils import confirm, entrypoint
@@ -18,3 +20,14 @@ def passwd(member: Member):
     confirm("Reset {}'s password?".format(member.crsid))
     membership.reset_password(member)
     print("Password changed")
+
+
+@entrypoint
+def cancel(sess: Session, member: Member):
+    """
+    Cancel a user account.
+
+    Usage: {script} MEMBER
+    """
+    confirm("Cancel {}?".format(member.name))
+    membership.cancel_member(sess, member)
