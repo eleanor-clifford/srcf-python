4d0f254f291dadba716e6c9f523fafa226d87b6d
Richard Allitt <rsa33@srcf.net>
2023-08-01 21:59:14 +0100

    Add unset-contactable flag to member cancellation

--- srcflib/scripts/member.py
+++ srcflib/scripts/member.py
@@ -24,11 +24,11 @@ def passwd(member: Member):
 
 
 @entrypoint
-def cancel(sess: Session, member: Member, unset_member: bool):
+def cancel(sess: Session, member: Member, unset_member: bool, unset_contactable: bool):
     """
     Cancel a user account.
 
-    Usage: {script} MEMBER [--unset-member]
+    Usage: {script} MEMBER [--unset-member] [--unset-contactable]
     """
     confirm("Cancel {}?".format(member.name))
-    membership.cancel_member(sess, member, unset_member)
+    membership.cancel_member(sess, member, unset_member, unset_contactable)
