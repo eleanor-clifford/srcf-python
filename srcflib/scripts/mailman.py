4302809ad646af3bd1663f5ef2e2315cc0cb7c48
Richard Allitt <rsa33@srcf.net>
2023-08-31 00:09:28 +0100

    Add sysadmin emails to Mailman tasks

--- srcflib/scripts/mailman.py
+++ srcflib/scripts/mailman.py
@@ -3,12 +3,13 @@ Scripts to manage Mailman mailing lists.
 """
 
 from .utils import confirm, DocOptArgs, entrypoint
+from ..email import send, SYSADMINS
 from ..plumbing.common import Owner
 from ..tasks import mailman
 
 
 @entrypoint
-def create(opts: DocOptArgs, owner: Owner):
+def create(opts: DocOptArgs, owner: Owner, suffix: str):
     """
     Create a Mailman mailing list.
 
@@ -16,29 +17,27 @@ def create(opts: DocOptArgs, owner: Owner):
 
     Usage: {script} OWNER [SUFFIX]
     """
-    name, admin = mailman._list_name_owner(owner, opts["SUFFIX"])
-    print("List address: {}@srcf.net{}".format(name, "" if opts["SUFFIX"] else " (!)"))
+    name, admin = mailman._list_name_owner(owner, suffix)
+    print("List address: {}@srcf.net{}".format(name, "" if suffix else " (!)"))
     print("Owner: {}".format(admin))
-    confirm("Create this list?")
-    result = mailman.create_list(owner, opts["SUFFIX"])
+    exists = suffix in mailman.get_list_suffixes(owner)
+    confirm("Reset this list's owner?" if exists else "Create this list?")
+    result = mailman.create_list(owner, suffix)
     if result:
         _, password = result.value
-        if password:
-            print("Created Mailman list {!r} owned by {}".format(name, admin))
-        else:
-            print("Reset owner of list {!r} to {}".format(name, admin))
+        send(SYSADMINS, "scripts/mailman_create.j2", {"name": name, "created": bool(password)})
 
 
 @entrypoint
-def delete(opts: DocOptArgs, owner: Owner):
+def delete(opts: DocOptArgs, owner: Owner, suffix: str):
     """
     Delete a Mailman mailing list, and optionally its archives.
 
     Usage: {script} OWNER [SUFFIX] [--archives]
     """
-    name, _ = mailman._list_name_owner(owner, opts["SUFFIX"])
+    name, _ = mailman._list_name_owner(owner, suffix)
     archives = opts["--archives"]
     print("List address: {}@srcf.net".format(name))
     confirm("Delete this list{}?".format(" and its archives" if archives else ""))
-    if mailman.remove_list(owner, opts["SUFFIX"], archives):
-        print("Deleted Mailman list {!r}".format(name))
+    if mailman.remove_list(owner, suffix, archives):
+        send(SYSADMINS, "scripts/mailman_delete.j2", {"name": name})
